90aee48635b5def092dfec5a40a56a9bff182fe5
shrutikatayde <shrutikatayde03@gmail.com>
2023-06-30 08:57:59 +0530

    modified

--- src/components/MovieHero/MovieHero.component.js
+++ src/components/MovieHero/MovieHero.component.js
@@ -11,11 +11,24 @@ const MovieHero = () => {
 
             </div>
 
-            <div className="hidden md:block">
+            <div className="hidden md:block lg:hidden">
+            <img
+                    src="https://assets-in.bmscdn.com/promotions/cms/creatives/1687845770319_fastxdesktop.jpg"
+                    alt=""
+                />
 
             </div>
 
-            <div className="hidden lg:block">
+            <div className="relative hidden lg:block" style={{ height: "30rem" }}>
+                
+                <div className="absolute h-full w-full z-10" style={{ backgroundImage: "linear-gradient(90deg, #1A1A1A 24.97%, #1A1A1A 38.3%, rgba(26, 26, 26, 0.0409746) 97.47%, #1A1A1A 100%" }}>
+                
+                </div>
+            <img
+                    src="https://assets-in.bmscdn.com/promotions/cms/creatives/1687845770319_fastxdesktop.jpg"
+                    alt=""
+                    className="w-full h-full"
+                />
 
             </div>
 
