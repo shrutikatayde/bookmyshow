00381b2313f507702a481a1819d8e85d04504ee8
SHRUTIKA TAYDE <89908129+shrutikatayde@users.noreply.github.com>
2023-07-13 14:24:31 +0530

    tmdb integration

--- src/pages/Home.Page.js
+++ src/pages/Home.Page.js
@@ -1,9 +1,20 @@
-import React from "react";
+
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import EntertainmentCardSlider from "../components/Entertainment/EntertainmentCard.component";
 import PosterSlider from "../components/PosterSlider/PosterSlider.component";
 import TempPoster from "../config/tempPosterConfig";
 
 const HomePage = () => {
+    const [popularMovies, setPopularMovies] = useState([]);
+    useEffect(() => {
+        const requestPopularMovies = async () => {
+            const getPopularMovies = await axios.get("/movie/popular");
+            setPopularMovies(getPopularMovies.data.results);
+        };
+        requestPopularMovies();
+    }, []);
+
     return (
         <>
             <div className=" flex flex-col gap-10">
@@ -21,10 +32,10 @@ const HomePage = () => {
                         <div className="flex">
                             <img src="https://assets-in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120:q-80/premiere-banner-web-collection-202208191200.png" alt="premier" className="w-full h-full py-4" />
                         </div>
-                        <PosterSlider images={TempPoster} title="Premiers" subtitle="Brand new release every Friday" isDark />
+                        <PosterSlider images={popularMovies} title="Premiers" subtitle="Brand new release every Friday" isDark />
                     </div>
                 </div>
-                
+
             </div>
 
             <div className="container mx-auto px-4">
